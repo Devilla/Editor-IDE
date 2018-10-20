2259b50a831337fa996de1d81cdf89ade6a0938a
devilla <dev.koold@gmail.com>
2018-10-19 21:10:57 -0700

    first demo

--- src/App.js
+++ src/App.js
@@ -7,19 +7,28 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+    
     this.state = {
       code: '// type your code... \n',
       message: 'hello',
-      chatbot: 'hi, how can i help?'
+      chatbot: 'ok',
+      messageData: [],
+      chatbotData: []
     }
+    this.state.chatbotData.push('hi, how can i help?');
   }
 
   onChange = (newValue, e) => {
-    console.log('onChange', newValue, e); // eslint-disable-line no-console
+    // console.log('onChange', newValue, e); // eslint-disable-line no-console
+    this.setState({ code: newValue });
+    console.log('CODE : ',this.state.code);
   }
   onSubmit = (e) => {
-    if(e.keyCode == 13)
-    console.log("Enter pressed"); // eslint-disable-line no-console
+    if(e.keyCode === 13) {
+    console.log("Enter pressed",e.target.value); // eslint-disable-line no-console
+    this.state.messageData.push(e.target.value);
+    this.state.chatbotData.push(this.state.chatbot);
+    }
   }
 
   editorDidMount = (editor) => {
@@ -42,6 +51,15 @@ class App extends Component {
     this.setState({ message: e.target.value });
   }
 
+  applyChange = () => {
+    // console.log('onChange', newValue, e); // eslint-disable-line no-console
+    // this.setState({ code: newValue });
+    // console.log('This CODE to be eval : ',eval(this.state.code));
+    this.state.chatbot = this.state.code.split('"', 2);
+    console.log("CHATBOT DATA :",this.state.chatbot[1]);
+    this.state.chatbot = this.state.chatbot[1];
+  }
+
   render() {
     const { code, message, chatbot } = this.state;
     const options = {
@@ -54,6 +72,7 @@ class App extends Component {
 
     return (
       <div className="App">
+      <button  className="Apply-Changes" onClick={this.applyChange}>Apply Changes</button>
         <header className="text-left">
         <MonacoEditor
           height="800"
@@ -67,19 +86,23 @@ class App extends Component {
         />
          <div className="Rectangle">
          <div className="Rectangle2">
-         <div className="Rectangle-4-Copy-2">{chatbot}</div>
-         <div className="Rectangle-4-Copy-3">{message}</div>
+         
+         { 
+            this.state.chatbotData.map((item, i) => {
+          
+            return <div><div key={i} className="Rectangle-4-Copy-2">{item}</div> 
+            <div key={i} className="Rectangle-4-Copy-3">{this.state.messageData[i]}</div></div>;
+          })
+         }
          </div>
          <input 
          placeholder="Type message here..." 
          className="Type-message-here" 
       
-         onChange={this.handleChange }
-         onKeyUp={this.onSubmit}/>
+         onChange={ this.handleChange }
+         onKeyUp={ this.onSubmit }/>
          </div>
-
         </header>
-       
       </div>
     );
   }
